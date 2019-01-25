0aaeb795b7644283e9cb28c656f09589dac616ef
Tejas <tsheth.p@gmail.com>
2019-01-25 22:55:07 +0530

    Update status_v2.py

--- status_v2.py
+++ status_v2.py
@@ -67,6 +67,7 @@ def get_scan(token,id):
         'content-type': "application/json",
     }
     r = requests.get('https://'+smart_check_url+'/api/scans/'+id, headers=headers, verify=False)
+    print(r.text)
     x = json.loads(r.text)
 
     if output == "status":
