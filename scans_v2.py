6960469adc80e4bb49836d4843c8fafee1d754b1
Tejas-CodeCommit <tsheth.p@gmail.com>
2019-01-26 21:26:11 +0530

    Revert "switch update"
    This reverts commit 881cabc2809b381d1f2d6e347f4c36f1ba0587dc.

--- scans_v2.py
+++ scans_v2.py
@@ -1,124 +1,148 @@
 #!/usr/bin/python
-import sys
-import getopt
-import os
+import sys, getopt
 import requests
 import urllib3
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 import json
 
-smart_check_url = ''
-smart_check_userid = ''
-smart_check_password = ''
-scan_id = ''
-output = 'status'
+smart_check_url=''
+smart_check_userid=''
+smart_check_password=''
+scan_registry=''
+scan_repository=''
+scan_tag='latest'
+aws_region='us-east-2'
+aws_id=''
+aws_secret=''
+scan_name='Python Script Scan'
+scan_id='v1'
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-def init(argv):
-    try:
-        opts, args = getopt.getopt(argv, "h:v",
-                                   ["smart_check_url=", "smart_check_userid=", "smart_check_password=", "scan_id=",
-                                    "output="])
+def init(argv): 
+   
+   try:
+      opts, args = getopt.getopt(argv,"h:v",["smart_check_url=","smart_check_userid=","smart_check_password=","scan_registry=","scan_repository=","scan_tag=","aws_region=","aws_id=","aws_secret=","scan_id="])
+   
+   except getopt.GetoptError as error:
+      print('Error Not enough Arguments')
+      print(str(error))
+      sys.exit(2)
 
-    except getopt.GetoptError as error:
-        print('Error Not enough Arguments')
-        print(str(error))
-        sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('scans.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("--smart_check_url"):
+         global smart_check_url
+         smart_check_url = arg
+         
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print('scans.py -i <inputfile> -o <outputfile>')
-            sys.exit()
-        elif opt in ("--smart_check_url"):
-            global smart_check_url
-            smart_check_url = arg
+      elif opt in ("--smart_check_userid"):
+        global smart_check_userid
+        smart_check_userid = arg
+        
+         
+      elif opt in ("--smart_check_password"):
+        global smart_check_password
+        smart_check_password = arg
 
-        elif opt in ("--smart_check_userid"):
-            global smart_check_userid
-            smart_check_userid = arg
+      elif opt in ("--scan_registry"):
+         global scan_registry
+         scan_registry = arg
+         
 
-        elif opt in ("--smart_check_password"):
-            global smart_check_password
-            smart_check_password = arg
+      elif opt in ("--scan_repository"):
+         global scan_repository
+         scan_repository = arg
+         
 
-        elif opt in ("--scan_id"):
-            global scan_id
-            scan_id = arg
+      elif opt in ("--scan_tag"):
+         global scan_tag
+         scan_tag = arg
+         
+         
+      elif opt in ("--aws_region"):
+         global aws_region
+         aws_region = arg
+         
+         
+      elif opt in ("--aws_id"):
+         global aws_id
+         aws_id = arg
+         
 
-        elif opt in ("--output"):
-            global output
-            output = arg
+      elif opt in ("--aws_secret"):
+         global aws_secret
+         aws_secret = arg
+         
+         
+      elif opt in ("--scan_name"):
+         global scan_name
+         scan_name = arg
+         
+      elif opt in ("--scan_id"):
+         global scan_id
+         scan_id = arg
 
-
-def get_token(userid, password):
-    # print("----- Generating Token ----- "+userid)
-    payload = {'user': {'userID': userid, 'password': password}}
-    r = requests.post('https://' + smart_check_url + '/api/sessions', json=payload, verify=False)
+         
+def get_token(userid,password):
+    #print("----- Generating Token ----- "+userid)
+    payload = {'user':{'userID': userid, 'password': password}}
+    r = requests.post('https://'+smart_check_url+'/api/sessions', json=payload, verify=False)
+    #print(r)
     z = json.loads(r.text)
+    #print(z['token'])
     return z
 
 
-def get_scan(token, id):
-    # print("----- Get Scan Data for "+id+" -----")
+def get_scan(token,id):
+    #print("----- Get Scan Data for "+id+" -----")
     headers = {
-        'authorization': "Bearer " + token,
-        'content-type': "application/json",
-    }
-    r = requests.get('https://' + smart_check_url + '/api/scans/' + id, headers=headers, verify=False)
+            'authorization': "Bearer " + token,
+            'content-type': "application/json",
+        }
+    r = requests.get('https://'+smart_check_url+'/api/scans/'+id, headers=headers, verify=False)
+    
     x = json.loads(r.text)
-
-    if output == "status":
-        print(x['status'])
+    #print(x['id'])
 
 
-    if output == "malware" and "malware" in x['findings']:
-        if (x['findings']['malware'] > 0):
-            print('malware_found')
-            # sys.exit(os.EX_SOFTWARE)
-        else:
-            print('no-malware')
-
-
-    if output == "contents" and "contents" in x['findings']:
-        try:
-            if (x['findings']['contents']['total']['high'] > 0):
-                print(str(x['findings']['contents']['total']['high']) + ' Secret stored found in image!')
-                # print('Critical Vulnerabilities Found')
-                # sys.exit(os.EX_SOFTWARE)
-            else:
-                print('No Secrets found in image')
-        except Exception as e:
-            pass
-
-
-    if output == "vulnerability" and "high" in x['findings']['vulnerabilities']['total']:
-            try:
-                if (x['findings']['vulnerabilities']['total']['high'] > 0):
-                    print(str(x['findings']['vulnerabilities']['total']['high']) + ' High Vulnerabilities Found!')
-                    # print('Critical Vulnerabilities Found')
-                    # sys.exit(os.EX_SOFTWARE)
-                else:
-                    print('No High Vulnerabilities')
-            except Exception as e:
-                pass
-
-            try:
-                if (x['findings']['vulnerabilities']['total']['critical'] > 0):
-                    print(
-                        str(x['findings']['vulnerabilities']['total']['critical']) + ' Critical Vulnerabilities Found!')
-                # print('Critical Vulnerabilities Found')
-                # sys.exit(os.EX_SOFTWARE)
-                else:
-                    print('No Critical Vulnerabilities')
-            except Exception as e:
-                pass
-
-
-
+def generate_request(token):
+    #print("----- Generating Request -----")
+    payload = {}
+    if scan_id:
+        #print("scan ID Not Empty ")
+        payload['id'] = scan_id
+    
+    
+    payload['name'] = scan_name
+    payload['source']={}
+    payload['source']['credentials']={}
+    payload['source']['credentials']['aws']={}
+    payload['source']['type']="docker"
+    payload['source']['registry']=scan_registry
+    payload['source']['repository']=scan_repository
+    payload['source']['tag']=scan_tag
+    payload['source']['credentials']['aws']['region']=aws_region
+    payload['source']['credentials']['aws']['accessKeyID']=aws_id
+    payload['source']['credentials']['aws']['secretAccessKey']=aws_secret
+    #print(payload)
+    headers = {
+            'authorization': "Bearer " + token,
+            'content-type': "application/json",
+        }
+    r = requests.post('https://'+smart_check_url+'/api/scans', json=payload, headers=headers, verify=False)
+    #print(r)
+    x = json.loads(r.text)
+    #print(x)
+    print(x['id'])
+    return x['id']
 
 init(sys.argv[1:])
-token = get_token(smart_check_userid, smart_check_password)
-# print(token)
-get_scan(token['token'], scan_id)
+#print(smart_check_userid)
+token = get_token(smart_check_userid,smart_check_password)
+#print (token['token'])
+scan_id = generate_request(token['token'])
+get_scan(token['token'],scan_id)
