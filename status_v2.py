30e26ef7cf3737e8448d332693af3547935e7ddc
Tejas <tsheth.p@gmail.com>
2019-01-26 20:43:38 +0530

    Update status_v2.py

--- status_v2.py
+++ status_v2.py
@@ -17,84 +17,88 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def init(argv):
+    try:
+        opts, args = getopt.getopt(argv, "h:v",
+                                   ["smart_check_url=", "smart_check_userid=", "smart_check_password=", "scan_id=",
+                                    "output="])
 
-   try:
-      opts, args = getopt.getopt(argv,"h:v",["smart_check_url=","smart_check_userid=","smart_check_password=","scan_id=","output="])
+    except getopt.GetoptError as error:
+        print('Error Not enough Arguments')
+        print(str(error))
+        sys.exit(2)
 
-   except getopt.GetoptError as error:
-      print('Error Not enough Arguments')
-      print(str(error))
-      sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('scans.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("--smart_check_url"):
+            global smart_check_url
+            smart_check_url = arg
 
-   for opt, arg in opts:
-      if opt == '-h':
-         print('scans.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("--smart_check_url"):
-         global smart_check_url
-         smart_check_url = arg 
-         
-      elif opt in ("--smart_check_userid"):
-        global smart_check_userid
-        smart_check_userid = arg
+        elif opt in ("--smart_check_userid"):
+            global smart_check_userid
+            smart_check_userid = arg
 
-      elif opt in ("--smart_check_password"):
-        global smart_check_password
-        smart_check_password = arg
+        elif opt in ("--smart_check_password"):
+            global smart_check_password
+            smart_check_password = arg
 
-      elif opt in ("--scan_id"):
-         global scan_id
-         scan_id = arg
+        elif opt in ("--scan_id"):
+            global scan_id
+            scan_id = arg
 
-      elif opt in ("--output"):
-         global output
-         output = arg
+        elif opt in ("--output"):
+            global output
+            output = arg
 
-def get_token(userid,password):
-    #print("----- Generating Token ----- "+userid)
-    payload = {'user':{'userID': userid, 'password': password}}
-    r = requests.post('https://'+smart_check_url+'/api/sessions', json=payload, verify=False)
-    #print(r.text)
+
+def get_token(userid, password):
+    # print("----- Generating Token ----- "+userid)
+    payload = {'user': {'userID': userid, 'password': password}}
+    r = requests.post('https://' + smart_check_url + '/api/sessions', json=payload, verify=False)
     z = json.loads(r.text)
-    # print(z['token'])
     return z
 
 
-def get_scan(token,id):
-    #print("----- Get Scan Data for "+id+" -----")
+def get_scan(token, id):
+    # print("----- Get Scan Data for "+id+" -----")
     headers = {
         'authorization': "Bearer " + token,
         'content-type': "application/json",
     }
-    r = requests.get('https://'+smart_check_url+'/api/scans/'+id, headers=headers, verify=False)
+    r = requests.get('https://' + smart_check_url + '/api/scans/' + id, headers=headers, verify=False)
     x = json.loads(r.text)
-    
+
     if output == "status":
         print(x['status'])
 
-        if output == "malware" and "malware" in x['findings']:
-            if(x['findings']['malware'] > 0):
-                print('malware_found')
-                #sys.exit(os.EX_SOFTWARE)
-            else:
-                print('no-malware')
 
+    if output == "malware" and "malware" in x['findings']:
+        if (x['findings']['malware'] > 0):
+            print('malware_found')
+            # sys.exit(os.EX_SOFTWARE)
+        else:
+            print('no-malware')
+
+
+    if output == "contents" and "contents" in x['findings']:
         try:
-            if(x['findings']['contents']['total']['high'] > 0):
+            if (x['findings']['contents']['total']['high'] > 0):
                 print(str(x['findings']['contents']['total']['high']) + ' Secret stored found in image!')
-                    #print('Critical Vulnerabilities Found')
-                    #sys.exit(os.EX_SOFTWARE)
+                # print('Critical Vulnerabilities Found')
+                # sys.exit(os.EX_SOFTWARE)
             else:
                 print('No Secrets found in image')
         except Exception as e:
             pass
 
-        if output == "critical" or "high" in x['findings']['vulnerabilities']['total']:
+
+    if output == "vulnerability" and "high" in x['findings']['vulnerabilities']['total']:
             try:
-                if(x['findings']['vulnerabilities']['total']['high'] > 0):
+                if (x['findings']['vulnerabilities']['total']['high'] > 0):
                     print(str(x['findings']['vulnerabilities']['total']['high']) + ' High Vulnerabilities Found!')
-                    #print('Critical Vulnerabilities Found')
-                    #sys.exit(os.EX_SOFTWARE)
+                    # print('Critical Vulnerabilities Found')
+                    # sys.exit(os.EX_SOFTWARE)
                 else:
                     print('No High Vulnerabilities')
             except Exception as e:
@@ -102,19 +106,19 @@ def get_scan(token,id):
 
             try:
                 if (x['findings']['vulnerabilities']['total']['critical'] > 0):
-                    print(str(x['findings']['vulnerabilities']['total']['critical']) + ' Critical Vulnerabilities Found!')
+                    print(
+                        str(x['findings']['vulnerabilities']['total']['critical']) + ' Critical Vulnerabilities Found!')
                 # print('Critical Vulnerabilities Found')
-                #sys.exit(os.EX_SOFTWARE)
+                # sys.exit(os.EX_SOFTWARE)
                 else:
                     print('No Critical Vulnerabilities')
             except Exception as e:
                 pass
 
-    else:
-        print('Overall Status')
-        #print(r.text)
+
+
 
 init(sys.argv[1:])
 token = get_token(smart_check_userid, smart_check_password)
-#print(token)
+# print(token)
 get_scan(token['token'], scan_id)
